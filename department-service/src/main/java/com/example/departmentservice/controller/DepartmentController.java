ee072a867f06256198cf81d5d62659ed2e9424d4
n1bil <nabil.nadzor@gmail.com>
2023-11-23 14:49:25 +0100

    Clean project for fresh start

---
+++ department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,33 @@
+package com.example.departmentservice.controller;
+
+import com.example.departmentservice.dto.DepartmentDto;
+import com.example.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    // Build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    // Build get department REST API
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode) {
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
+    }
+
+}
